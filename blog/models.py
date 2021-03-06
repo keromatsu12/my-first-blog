d47db3646c6363a764c47dd50b93c2c3ad9990ff
kazuya kanematsu <kazu.dance.12@gmail.com>
2021-03-06 16:33:23 +0900

    入力フォームの作成とそのCSSファイルの追加

--- blog/models.py
+++ blog/models.py
@@ -2,14 +2,15 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-
-class Post(models.Model):
+# ブログの投稿を管理するクラス
+class Post(models.Model): #(models.Model) -> Djangoがデータベースに保存すべきもの
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    text = models.TextField()
+    title = models.CharField(max_length=200)            #文字数が制限されたテキストを定義
+    text = models.TextField()                           #文字数が制限されないテキストを定義
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
+    # ブログの投稿機能
     def publish(self):
         self.published_date = timezone.now()
         self.save()
